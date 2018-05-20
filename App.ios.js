18f11fb380bb444f87f4e17bd0375331c35214f1
SalmaAli <salmaali@SalmaAlis-MacBook-Pro.local>
2018-05-20 11:20:52 +0200

    added styling for the list displayed in the ios app

--- App.ios.js
+++ App.ios.js
@@ -10,62 +10,106 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  ScrollView,
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native';
 import NetworkManager from 'react-native-ibgx-network-manager';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      list: [],
+      isLoading: false
+    };
+  }
+
+  componentDidMount() {
+    this.getRequest();
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <TouchableOpacity onPress={() => this.getRequest()}>
-        <Text>Get</Text>
-        </TouchableOpacity>
+        {!this.state.error && (
+          <ScrollView style={styles.listContainer}>
+            {this.state.list.map((item, i) => (
+              <View key={i} style={styles.itemContainer}>
+                <Text>{item.title}</Text>
+                <View style={styles.separator} />
+              </View>
+            ))}
+          </ScrollView>
+        )}
+        {!this.state.error && (
+          <View style={styles.errorContainer}>
+            <Text>{this.state.error}</Text>
+            <TouchableOpacity onPress={() => this.getRequest()}>
+              <Text>Try Again!</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
 
   getRequest() {
-    NetworkManager.get('https://www.propertyfinder.ae/mobileapi?page=[0]&order=[pd]', (error, res) => {
-      alert(res);
-      alert(error);
-    });
+    this.setState({ isLoading: true });
+    NetworkManager.get(
+      'https://www.propertyfinder.ae/mobileapi?page=0&order=pd',
+      (error, res) => {
+        if (error === null) {
+          this.setState({ error, isLoading: false });
+        }
+        // alert(JSON.stringify(res.res[0]))
+        this.setState({ list: res.res, isLoading: false, error: null });
+      }
+    );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  listContainer: {
+    marginTop: 30
+  },
+  itemContainer: {
+    padding: 20
+  },
+  separator: {
+    height: StyleSheet.hairlineWidth,
+    width: '100%',
+    backgroundColor: 'grey',
+    marginTop: 20
+  },
+  loadingContainer: {
+    flex: 1,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
